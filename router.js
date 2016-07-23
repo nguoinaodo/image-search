6020cfde98ddc71dd66ba613c2ea3d15d4160ce1
LuuHoang <luugu196@gmail.com>
2016-07-23 16:38:46 +0000

    commit

--- router.js
+++ router.js
@@ -1,5 +1,6 @@
 var https = require("https");
 var express = require("express");
+var bl = require('bl');
 var router = express.Router();
 
 router.use('/api/imagesearch', function(req, res, next) {
@@ -8,7 +9,7 @@ router.use('/api/imagesearch', function(req, res, next) {
     var option = {
         protocol: 'https:',
         host: 'api.cognitive.microsoft.com',
-        path: '/bing/v5.0/images/search?q=' + params[0] + '&count=1&' + params[1] + '&safeSearch=Moderate',
+        path: '/bing/v5.0/images/search?q=' + params[0] + '&count=5&' + params[1] + '&safeSearch=Moderate',
         method: 'GET',
         headers: {
             'Ocp-Apim-Subscription-Key': 'f16ea4b024cd47a3b23de993ff14ced7'
@@ -16,15 +17,11 @@ router.use('/api/imagesearch', function(req, res, next) {
     };
     var result = [];
     
-    var request = https.request(option, function(response) {
-        response.on('error', function(err) {
-            console.log('Error:' + err);
-        });
-        response.on('data', function(chunk) {
-            var data = JSON.parse(chunk);
-            console.log(data);
-            console.log('hi');
-            data.value.forEach(function(item) {
+    var request = https.get(option, function(response) {
+        response.pipe(bl(function(err, data) {
+            if (err) throw err;
+            var json = JSON.parse(data.toString());
+            json.value.forEach(function(item) {
                 result.push({
                     url: item.contentUrl,
                     snippet: item.name,
@@ -33,15 +30,8 @@ router.use('/api/imagesearch', function(req, res, next) {
                 });
             });
             res.json(result);
-        });
-        response.on('end', function() {
-            console.log('no more data');
-        });
-    }).on('error', function(err) {
-        console.log(err);
+        }));
     });
-    request.write('data');
-    request.end();
 });
 
 ///////////
